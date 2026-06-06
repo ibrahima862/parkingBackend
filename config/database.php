0e60af096c4bd8630449eb634d935676c72cb468
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 20:12:09 +0000

    derniere commit

--- config/database.php
+++ config/database.php
@@ -149,7 +149,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
@@ -180,5 +180,9 @@ return [
         ],
 
     ],
+
+    'options' => extension_loaded('pdo_mysql') ? array_filter([
+        PDO::MYSQL_ATTR_SSL_CA => true,
+    ]) : [],
 
 ];
