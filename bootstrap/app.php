5151c825fd1310d9dbe3bae932c24e282764dd1c
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 23:29:07 +0000

    Fix Activity log crash and safe check in approveParking

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         
-        // AJOUTEZ LE MIDDLEWARE CORS ICI
+        
         $middleware->append(HandleCors::class);
 
         $middleware->validateCsrfTokens(except: [
