d19dcf53b250f5a645b7e08d295610642d8b171d
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 21:19:43 +0000

    Fix route login 404 et ouverture complete du CORS

--- config/cors.php
+++ config/cors.php
@@ -3,16 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => 
-    [
-        'http://localhost:5173', 
-        'https://indissolubly-unmediating-tressa.ngrok-free.dev',
-        'http://127.0.0.1:8000',
-        'http://192.168.1.2:8000',
-        'https://parking-frontend-3ofn5wpc0-ibrahima-s-projects4.vercel.app',
-        'http://192.168.1.2:5173',
-        'https://parkingbackend-4wcy.onrender.com'
-        ],
+    'allowed_origins' => ['*'], 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
