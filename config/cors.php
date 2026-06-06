beb73e84dc9974d0e7d3d1d83ba141239fedd409
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 20:00:59 +0000

    add vercel link into cors

--- config/cors.php
+++ config/cors.php
@@ -9,6 +9,7 @@ return [
         'https://indissolubly-unmediating-tressa.ngrok-free.dev',
         'http://127.0.0.1:8000',
         'http://192.168.1.2:8000',
+        'https://parking-frontend-3ofn5wpc0-ibrahima-s-projects4.vercel.app',
         'http://192.168.1.2:5173'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
