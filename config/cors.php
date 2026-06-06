68d0f5383bcd68361c68ebc694296f80bc781678
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 21:14:05 +0000

    Envoie du fichier cors

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,7 @@ return [
         'http://192.168.1.2:8000',
         'https://parking-frontend-3ofn5wpc0-ibrahima-s-projects4.vercel.app',
         'http://192.168.1.2:5173',
-        'https://parkingbackend-4wcy.onrender.com'],
+        ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
