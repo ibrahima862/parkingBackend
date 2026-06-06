4475529098b87c42131d8f2d1d7ebbed1275d6bb
Ibrahima Ndoye <ndoyeibrahima512@gmail.com>
2026-06-06 23:26:03 +0000

    Fix Activity log crash and safe check in approveParking

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors; 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,10 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    
+        
+        // AJOUTEZ LE MIDDLEWARE CORS ICI
+        $middleware->append(HandleCors::class);
+
         $middleware->validateCsrfTokens(except: [
             'api/*', 
         ]);
+        
         $middleware->redirectGuestsTo(fn () => response()->json([
             'success' => false,
             'message' => 'Non authentifié. Veuillez fournir un token valide.'
